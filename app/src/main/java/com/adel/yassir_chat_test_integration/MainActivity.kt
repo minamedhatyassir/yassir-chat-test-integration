e4292aaa6b3c135b6573dff44f7a2a0f5e1bac51
Adel El-Hawary <adeltarek.088@gmail.com>
2023-04-03 17:59:37 +0200

    init commit

---
+++ app/src/main/java/com/adel/yassir_chat_test_integration/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.adel.yassir_chat_test_integration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
